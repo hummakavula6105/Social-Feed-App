685fc7a20fbbf0f3a3e61ac45d59c05e88b2571c
Danielle <hummakavula6105@gmail.com>
2022-09-11 19:45:53 -0400

    Imported functions into App.js and created structure for posts

--- social-feed/src/App.js
+++ social-feed/src/App.js
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
 import DisplayPosts from "./components/DisplayPosts/DisplayPosts";
+import AddPost from "./components/Posts/CreatePost";
+import Post from "./components/Posts/Post";
 
 function App() {
   const [entries, setEntries] = useState([
     {
       name: 'name',
-      post: 'post',
-    }
+      post: 'post'
+    }])
 
-  ])
+  function addNewPost(entry) {
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
-    <div className="App">
-        Hello World!
-        <DisplayPosts parentEntries={entries}/>
-
+    <div className='container-fluid'>
+      <div className='row'>
+        <h3 style={{margin: '1em'}}>Weight
+        <small className='text-muted'>Tracker</small></h3>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <DisplayPosts parentEntries={entries} />
+          </div>
+          <div className='border-box'>
+            <CreatePost addNewPostProperty={addNewPost} />
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <Post  parentPosts={entry} />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-
 export default App;
